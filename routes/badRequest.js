ef963b2cd45d804d4efa24a9855d45626c012cad
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-01-15 00:04:37 +0300

    Feat: Show errors for bad request methods

--- routes/badRequest.js
+++ routes/badRequest.js
@@ -1,7 +1,11 @@
 const badRequestRrouter = require('express').Router();
 
 badRequestRrouter.get('/*', (req, res) => {
-  res.status(404).send({ 'message': "Запрашиваемый ресурс не найден" });
+  res.status(404).json({ 'message': "Запрашиваемый ресурс не найден" });
+});
+
+badRequestRrouter.use('/*', (req, res) => {
+  res.status(404).json({ 'message': `Невозможно выполнить действие ${req.method} для http://localhost:3000${req.path + req.params[0]}` });
 });
 
 module.exports = badRequestRrouter;
