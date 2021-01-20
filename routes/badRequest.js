46d6f0377a60a5f73812853525e4e2d173653404
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-01-20 21:16:42 +0300

    Fix eslint issues

--- routes/badRequest.js
+++ routes/badRequest.js
@@ -1,9 +1,10 @@
 const badRequestRrouter = require('express').Router();
 
+badRequestRrouter.get('/*', (req, res) => {
+  res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 badRequestRrouter.use('/*', (req, res) => {
-  if (req.method === 'GET') {
-    res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
-  }
   res.status(404).json({ message: `Невозможно выполнить действие ${req.method} для http://localhost:3000${req.path + req.params[0]}` });
 });
 
