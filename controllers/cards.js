ed827f853d5adc02ef3749c4d16e4fb9c51a309d
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-02-03 12:52:34 +0300

    Feat: Add likeCard dislikeCard options

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,3 +22,24 @@ module.exports.deleteCard = (req, res) => {
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
 };
+
+module.exports.likeCard = (req, res) => {
+  console.log(req.user._id);
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка: ${err}` }));
+};
--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,5 @@
 const cardsRouter = require('express').Router();
-const { getCards, createCard, deleteCard } = require('../controllers/cards');
+const { getCards, createCard, deleteCard, likeCard, dislikeCard } = require('../controllers/cards');
 
 // Запрос списка карточек
 cardsRouter.get('/cards', getCards);
@@ -7,7 +7,13 @@ cardsRouter.get('/cards', getCards);
 // Запрос на создание карточки
 cardsRouter.post('/cards', createCard);
 
-// Запрос на создание карточки
+// Запрос на удаление карточки
 cardsRouter.delete('/cards/:cardId', deleteCard);
+
+// Запрос на добавление лайка карточке
+cardsRouter.put('/cards/:cardId/likes', likeCard);
+
+// Запрос на удаление лайка с карточки
+cardsRouter.delete('/cards/:cardId/likes', dislikeCard);
 
 module.exports = cardsRouter;
